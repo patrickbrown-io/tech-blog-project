26275996cfe4a2465890d9765fcf3f61ba978ae0
Patrick Brown <breepatbree@gmail.com>
2021-09-18 15:59:31 -0600

    Added comment functionality

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
+// let JAWSDB_URL= "mysql://ti3m9lsy9ghgw82u:d0m5vdl2srlirr4t@kfgk8u2ogtoylkq9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/nfglo4izdllfyt4v";
+
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
